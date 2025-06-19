280f6970ff373a6db75149957f2922a29451d8dd
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2025-06-19 08:26:58 +0530

    Update app.py

--- argri proj/app.py
+++ argri proj/app.py
@@ -12,32 +12,7 @@ app = Flask(__name__)
 def home():
     return "🌾 Crop Recommendation API is running!"
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.get_json(force=True)
 
-    try:
-        # Extract features from input JSON
-        features = [
-            data["N"],
-            data["P"],
-            data["K"],
-            data["temperature"],
-            data["humidity"],
-            data["ph"],
-            data["rainfall"]
-        ]
-
-        # Convert to NumPy array
-        input_array = np.array([features])
-
-        # Predict using model
-        prediction = model.predict(input_array)[0]
-
-        return jsonify({"recommended_crop": prediction})
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
